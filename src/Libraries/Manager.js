79ccd630500cac0af8e163458ad3e7a1bf5b1157
abusaddique95 <abusaddique95@live.com>
2022-06-15 11:21:01 +0100

    classes for manager and render html

---
+++ src/Libraries/Manager.js
@@ -0,0 +1,38 @@
+// import employee
+
+const Employee = require("./Employee");
+
+// add class and extend to employee
+
+class Manager extends Employee {
+  constructor({ name, email, id, officeNumber }) {
+    {
+      super({ name, id, email });
+    }
+    this.officeNumber = officeNumber;
+    this.role = "Manager";
+  }
+
+  generateOfficeNumber() {
+    return this.generateOfficeNumber;
+  }
+
+  // render the HTML
+
+  generateManagerHTML() {
+    return `<div class="card m-3 bg-warning" style="width: 18rem;">
+    <div class="card-body text-center">
+        <h5 class="card-title">${this.name}</h5>
+        <h6>Manager</h6>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">${this.id}</li>
+        <li class="list-group-item">Email:
+            <a href="mailto:${this.email}" target="_blank" class="card-link">${this.email}/a>
+        <li class="list-group-item">${this.officeNumber}</li>
+    </ul>
+</div>`;
+  }
+}
+
+module.exports = Manager;
